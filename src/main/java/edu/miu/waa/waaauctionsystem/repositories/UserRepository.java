b31e53c47bf71192d1686294b48b31247fef4fde
Rdev90 <dorminvilroosvelt@gmail.com>
2023-10-23 13:12:46 -0500

    commit wih users controller

--- src/main/java/edu/miu/waa/waaauctionsystem/repositories/UserRepository.java
+++ src/main/java/edu/miu/waa/waaauctionsystem/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package edu.miu.waa.waaauctionsystem.repositories;
 
 import edu.miu.waa.waaauctionsystem.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+    public Page<User> findAll(Pageable pageable);
     public Optional<User> findByEmail(String email);
 }
--- src/main/java/edu/miu/waa/waaauctionsystem/services/UserService.java
+++ src/main/java/edu/miu/waa/waaauctionsystem/services/UserService.java
@@ -1,12 +1,13 @@
 package edu.miu.waa.waaauctionsystem.services;
 
 import edu.miu.waa.waaauctionsystem.models.User;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    public List<User> getAll();
+    public Page<User> getAll(int page, int pageSize);
     public Optional<User> getById(Long id);
     public Optional<User> getByEmail(String email);
     public User creatUser(User user);
